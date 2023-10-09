9ae34f198d36df26f3e2936033324724ac08c93e
Igor Thierry <igorthierry15@gmail.com>
2023-10-09 10:09:46 -0300

    Carregando dados do curso

--- src/store/slices/player.spec.ts
+++ src/store/slices/player.spec.ts
@@ -1,22 +1,31 @@
 import { describe, expect, it } from 'vitest'
-import { player as reducer, play, next } from "./player";
+import { player as reducer, play, next, PlayerState } from './player'
 
-const exampleState = {
+const exampleState: PlayerState = {
   course: {
+    id: 1,
     modules: [
       {
-        id: '1',
+        id: 1,
         title: 'Iniciando com React',
         lessons: [
           { id: 'Jai8w6K_GnY', title: 'CSS Modules', duration: '13:45' },
-          { id: 'w-DW4DhDfcw', title: 'Estilização do Post', duration: '10:05' },
+          {
+            id: 'w-DW4DhDfcw',
+            title: 'Estilização do Post',
+            duration: '10:05',
+          },
         ],
       },
       {
-        id: '2',
+        id: 2,
         title: 'Estrutura da aplicação',
         lessons: [
-          { id: 'gE48FQXRZ_o', title: 'Componente: Comment', duration: '13:45' },
+          {
+            id: 'gE48FQXRZ_o',
+            title: 'Componente: Comment',
+            duration: '13:45',
+          },
           { id: 'Ng_Vk4tBl0g', title: 'Responsividade', duration: '10:05' },
         ],
       },
@@ -42,23 +51,29 @@ describe('player slice', () => {
   })
 
   it('should be able to jump to the next module automatically', () => {
-    const state = reducer({
-      ...exampleState,
-      currentLessonIndex: 1
-    }, next())
+    const state = reducer(
+      {
+        ...exampleState,
+        currentLessonIndex: 1,
+      },
+      next(),
+    )
 
     expect(state.currentModuleIndex).toEqual(1)
     expect(state.currentLessonIndex).toEqual(0)
   })
 
   it('should not update the current module and lesson index if there is not next lesson available', () => {
-    const state = reducer({
-      ...exampleState,
-      currentModuleIndex: 1,
-      currentLessonIndex: 1
-    }, next())
+    const state = reducer(
+      {
+        ...exampleState,
+        currentModuleIndex: 1,
+        currentLessonIndex: 1,
+      },
+      next(),
+    )
 
     expect(state.currentModuleIndex).toEqual(1)
     expect(state.currentLessonIndex).toEqual(1)
   })
-});
+})
